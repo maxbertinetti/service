39e86a580aeb3585c529b82c54c2fcd554196218
Massimiliano Bertinetti <max@eclecticlly.com>
2021-09-08 14:53:12 +0200

    feat: new version after course

---
+++ app/services/sales-api/handlers/handlers.go
@@ -0,0 +1,138 @@
+// Package handlers contains the full set of handler functions and routes
+// supported by the web api.
+package handlers
+
+import (
+	"context"
+	"expvar"
+	"net/http"
+	"net/http/pprof"
+	"os"
+
+	"github.com/ardanlabs/service/app/services/sales-api/handlers/debug/checkgrp"
+	v1ProductGrp "github.com/ardanlabs/service/app/services/sales-api/handlers/v1/productgrp"
+	v1UserGrp "github.com/ardanlabs/service/app/services/sales-api/handlers/v1/usergrp"
+	productCore "github.com/ardanlabs/service/business/core/product"
+	userCore "github.com/ardanlabs/service/business/core/user"
+	"github.com/ardanlabs/service/business/sys/auth"
+	"github.com/ardanlabs/service/business/web/mid"
+	"github.com/ardanlabs/service/foundation/web"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
+
+// Options represent optional parameters.
+type Options struct {
+	corsOrigin string
+}
+
+// WithCORS provides configuration options for CORS.
+func WithCORS(origin string) func(opts *Options) {
+	return func(opts *Options) {
+		opts.corsOrigin = origin
+	}
+}
+
+// DebugStandardLibraryMux registers all the debug routes from the standard library
+// into a new mux bypassing the use of the DefaultServerMux. Using the
+// DefaultServerMux would be a security risk since a dependency could inject a
+// handler into our service without us knowing it.
+func DebugStandardLibraryMux() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Register all the standard library debug endpoints.
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	mux.Handle("/debug/vars", expvar.Handler())
+
+	return mux
+}
+
+// DebugMux registers all the debug standard library routes and then custom
+// debug application routes for the service. This bypassing the use of the
+// DefaultServerMux. Using the DefaultServerMux would be a security risk since
+// a dependency could inject a handler into our service without us knowing it.
+func DebugMux(build string, log *zap.SugaredLogger, db *sqlx.DB) http.Handler {
+	mux := DebugStandardLibraryMux()
+
+	// Register debug check endpoints.
+	cgh := checkgrp.Handlers{
+		Build: build,
+		Log:   log,
+		DB:    db,
+	}
+	mux.HandleFunc("/debug/readiness", cgh.Readiness)
+	mux.HandleFunc("/debug/liveness", cgh.Liveness)
+
+	return mux
+}
+
+// APIMuxConfig contains all the mandatory systems required by handlers.
+type APIMuxConfig struct {
+	Shutdown chan os.Signal
+	Log      *zap.SugaredLogger
+	Auth     *auth.Auth
+	DB       *sqlx.DB
+}
+
+// APIMux constructs an http.Handler with all application routes defined.
+func APIMux(cfg APIMuxConfig, options ...func(opts *Options)) http.Handler {
+	var opts Options
+	for _, option := range options {
+		option(&opts)
+	}
+
+	// Construct the web.App which holds all routes as well as common Middleware.
+	app := web.NewApp(
+		cfg.Shutdown,
+		mid.Logger(cfg.Log),
+		mid.Errors(cfg.Log),
+		mid.Metrics(),
+		mid.Panics(),
+	)
+
+	// Accept CORS 'OPTIONS' preflight requests if config has been provided.
+	// Don't forget to apply the CORS middleware to the routes that need it.
+	// Example Config: `conf:"default:https://MY_DOMAIN.COM"`
+	if opts.corsOrigin != "" {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			return nil
+		}
+		app.Handle(http.MethodOptions, "", "/*", h)
+	}
+
+	// Load the routes for the different versions of the API.
+	v1(app, cfg)
+
+	return app
+}
+
+// v1 binds all the version 1 routes.
+func v1(app *web.App, cfg APIMuxConfig) {
+	const version = "v1"
+
+	// Register user management and authentication endpoints.
+	ugh := v1UserGrp.Handlers{
+		User: userCore.NewCore(cfg.Log, cfg.DB),
+		Auth: cfg.Auth,
+	}
+	app.Handle(http.MethodGet, version, "/users/token", ugh.Token)
+	app.Handle(http.MethodGet, version, "/users/:page/:rows", ugh.Query, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodGet, version, "/users/:id", ugh.QueryByID, mid.Authenticate(cfg.Auth))
+	app.Handle(http.MethodPost, version, "/users", ugh.Create, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodPut, version, "/users/:id", ugh.Update, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodDelete, version, "/users/:id", ugh.Delete, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
+
+	// Register product and sale endpoints.
+	pgh := v1ProductGrp.Handlers{
+		Product: productCore.NewCore(cfg.Log, cfg.DB),
+	}
+	app.Handle(http.MethodGet, version, "/products/:page/:rows", pgh.Query, mid.Authenticate(cfg.Auth))
+	app.Handle(http.MethodGet, version, "/products/:id", pgh.QueryByID, mid.Authenticate(cfg.Auth))
+	app.Handle(http.MethodPost, version, "/products", pgh.Create, mid.Authenticate(cfg.Auth))
+	app.Handle(http.MethodPut, version, "/products/:id", pgh.Update, mid.Authenticate(cfg.Auth))
+	app.Handle(http.MethodDelete, version, "/products/:id", pgh.Delete, mid.Authenticate(cfg.Auth))
+}
---
+++ business/core/report/report.go
@@ -0,0 +1,41 @@
+// Package report provides an example of a core business API.
+package report
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ardanlabs/service/business/data/store/product"
+	"github.com/ardanlabs/service/business/data/store/user"
+	"github.com/ardanlabs/service/business/sys/auth"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
+
+// Core manages the set apis for report functionality.
+type Core struct {
+	User    user.Store
+	Product product.Store
+}
+
+// NewCore constructs a core for report api access.
+func NewCore(log *zap.SugaredLogger, db *sqlx.DB) Core {
+	return Core{
+		User:    user.NewStore(log, db),
+		Product: product.NewStore(log, db),
+	}
+}
+
+// UserProducts validates the user exists and returns products they have created.
+func (c Core) UserProducts(ctx context.Context, claims auth.Claims, userID string) ([]product.Product, error) {
+	if _, err := c.User.QueryByID(ctx, claims, userID); err != nil {
+		return nil, fmt.Errorf("query user UserID[%s]: %w", userID, err)
+	}
+
+	products, err := c.Product.QueryByUserID(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("query products UserID[%s]: %w", userID, err)
+	}
+
+	return products, nil
+}
